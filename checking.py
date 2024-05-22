5c8798fc1dc4dba22bf1c4be426b8dfd754e3f71
Viratsingh527 <146914639+Viratsingh527@users.noreply.github.com>
2024-05-22 12:17:00 +0530

    experimenting on differnet image

--- checking.py
+++ checking.py
@@ -6,10 +6,11 @@ import requests
 pipe = pipeline(task="depth-estimation", model="LiheYoung/depth-anything-large-hf")
 
 # load image
-url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+url = '/DATA2/mayur/pawscan/code/babu/datasets/CASIA-B/CASIA-B/GaitDatasetB-silh/001/bg-01/000'
 image = Image.open(requests.get(url, stream=True).raw)
 
 # inference
 depth = pipe(image)["depth"]
+print('url of image :',url)
 print("Data type of image:", type(image))
 print("Data type of depth:", type(depth))
